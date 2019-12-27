ad38b75c9a43ab49ac9985838000f4b03eca6fe8
dillan <dillanspencer@uvic.ca>
2019-12-27 14:25:54 -0800

    Update minimax

--- src/main/java/com/battlesnake/board/Board.java
+++ src/main/java/com/battlesnake/board/Board.java
@@ -175,12 +175,45 @@ public class Board {
     public Move getMove() {
         int x = you().getHead().getX();
         int y = you().getHead().getY();
+        int[] score = {0, 0, 0, 0};
+        int best = Board.MIN;
+        Move move = Move.UP;
+        List<Move> possibleMoves = getPossibleMoves(you().getHead());
 
+        for(int i = 0; i < possibleMoves.size() - 1; i++){
+            Snake s = you();
+            if (possibleMoves.get(i).equals(Move.UP)) {
+               s.getHead().setY(s.getHead().getY() - 1);
+               score[0] = minimax(board, 0, false, s, snakes.get(0), Board.MAX, Board.MIN);
+               if(score[0] > best){
+                   move = Move.UP;
+                   best = score[0];
+               }
+            } else if (possibleMoves.get(i).equals(Move.DOWN)) {
+                s.getHead().setY(s.getHead().getY() + 1);
+                score[1] = minimax(board, 0, false, s, snakes.get(0), Board.MAX, Board.MIN);
+                if(score[1] > best){
+                    move = Move.DOWN;
+                    best = score[1];
+                }
+            } else if (possibleMoves.get(i).equals(Move.LEFT)) {
+                s.getHead().setX(s.getHead().getX() - 1);
+                score[2] = minimax(board, 0, false, s, snakes.get(0), Board.MAX, Board.MIN);
+                if(score[2] > best){
+                    move = Move.LEFT;
+                    best = score[2];
+                }
+            } else if (possibleMoves.get(i).equals(Move.RIGHT)) {
+                s.getHead().setX(s.getHead().getX() + 1);
+                score[3] = minimax(board, 0, false, s, snakes.get(0), Board.MAX, Board.MIN);
+                if(score[3] > best){
+                    move = Move.RIGHT;
+                    best = score[3];
+                }
+            }
+        }
 
-        int score = minimax(board, 0, false, you(), snakes.get(0), Board.MIN, Board.MAX);
-        System.out.println(score);
-
-        return Move.DOWN;
+        return move;
     }
 
     public void init(Snake you) {
