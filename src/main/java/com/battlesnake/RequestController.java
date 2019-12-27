cc9d60840f57e9713c9d3cbf9bc46675314e37ed
dillan <dillanspencer@uvic.ca>
2019-12-27 15:19:38 -0800

    Logging Test

--- src/main/java/com/battlesnake/RequestController.java
+++ src/main/java/com/battlesnake/RequestController.java
@@ -46,7 +46,7 @@ public class RequestController {
 
         List<Move> towardsFoodMoves = moveTowardsFood(request, mySnake.getBody().get(0));
 
-        if(mySnake.getHealth() < 30) {
+        if(mySnake.getHealth() < 10) {
             if (towardsFoodMoves != null && !towardsFoodMoves.isEmpty()) {
                 return moveResponse.setMove(towardsFoodMoves.get(0)).setTaunt("I'm hungry");
             }
