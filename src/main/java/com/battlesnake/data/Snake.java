96fe4cc0984b01813ea77cf2c1f3bb9d96ed5c3a
dillan <dillanspencer@uvic.ca>
2020-02-10 18:50:52 -0800

    Update Snake.java

--- src/main/java/com/battlesnake/data/Snake.java
+++ src/main/java/com/battlesnake/data/Snake.java
@@ -85,7 +85,7 @@ public class Snake {
        }else if(length() > board.longestSnake() && Point.distance(getHead(), enemy.getHead()) < 2){
            System.out.println("AGRESSIVE");
            return SnakeState.AGRESSIVE;
-       }else if(Point.distance(getHead(), enemy.getHead()) < 2) {
+       }else if(Point.distance(getHead(), enemy.getHead()) < 4) {
            System.out.println("SMART");
            return SnakeState.SMART;
        }
