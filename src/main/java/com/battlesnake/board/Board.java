0bdab85d95c25ceb9c2bb13226df4d48db0717dd
Dillan Spencer <46987282+DillanSpencer@users.noreply.github.com>
2020-11-09 19:07:13 -0800

    Update Board.java

--- src/main/java/com/battlesnake/board/Board.java
+++ src/main/java/com/battlesnake/board/Board.java
@@ -251,7 +251,7 @@ public class Board {
         if (!exists(point)) return true;
         return board[point.getX()][point.getY()] != Tile.EMPTY
                 && board[point.getX()][point.getY()] != Tile.FOOD
-                && board[point.getX()][point.getY()] != Tile.TAIL
+                //&& board[point.getX()][point.getY()] != Tile.TAIL
                 && board[point.getX()][point.getY()] != Tile.FAKE_WALL
                 && board[point.getX()][point.getY()] != Tile.HEADS;
     }
