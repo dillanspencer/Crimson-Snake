49bdcf7a22d0cbb4326653a339ad375f4446ad67
Dillan Spencer <46987282+DillanSpencer@users.noreply.github.com>
2020-11-10 15:19:54 -0800

    Update Board.java

--- src/main/java/com/battlesnake/board/Board.java
+++ src/main/java/com/battlesnake/board/Board.java
@@ -367,6 +367,7 @@ public class Board {
                     bestMove = returnMove;
                 }
                 if (beta <= alpha) {
+                    System.out.println("Beta <= Alpha: " + beta + ", " + alpha);
                     bestMove.returnValue = beta;
                     bestMove.returnMove = null;
                     return bestMove; // pruning
@@ -394,6 +395,7 @@ public class Board {
                     bestMove = returnMove;
                 }
                 if (beta <= alpha) {
+                    System.out.println("Beta <= Alpha: " + beta + ", " + alpha);
                     bestMove.returnValue = alpha;
                     bestMove.returnMove = null;
                     return bestMove; // pruning
