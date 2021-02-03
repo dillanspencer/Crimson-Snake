c3c384d7512e213f2fae6cffa2080db1e4dead1e
Dillan Spencer <46987282+DillanSpencer@users.noreply.github.com>
2021-02-02 22:54:05 -0800

    Update Pathfinding.java

--- src/main/java/com/battlesnake/pathfinding/Pathfinding.java
+++ src/main/java/com/battlesnake/pathfinding/Pathfinding.java
@@ -134,7 +134,8 @@ public class Pathfinding {
         if (nextX >= 0 && nextX < maxWidth) {
             if (nextY >= 0 && nextY < maxHeight) {
                 return tiles[nextX][nextY].isOpen() &&
-                        tiles[nextX][nextY].getTileType() != TileType.WALL;
+                        tiles[nextX][nextY].getTileType() != TileType.WALL &&
+                        tiles[nextX][nextY].getTileType() != TileType.FAKE_WALL;
             }
         }
         return false;
