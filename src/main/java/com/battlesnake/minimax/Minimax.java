4bd2f9ed67c3fc29ef83becc5c7ee7e2a306f47b
Dillan Spencer <46987282+DillanSpencer@users.noreply.github.com>
2021-03-15 17:07:20 -0700

    Update Minimax.java

--- src/main/java/com/battlesnake/minimax/Minimax.java
+++ src/main/java/com/battlesnake/minimax/Minimax.java
@@ -66,13 +66,11 @@ public class Minimax {
             List<Move> moves = getPossibleMoves(mySnake.getHead(), true);
             if(moves.size() <= 0) System.out.println("Well fuck");
             for (Move currentMove : moves) {
-                clearSnake(mySnake);
                 mySnake.applyMove(currentMove);
                 updateBoard(mySnake);
                 returnMove = maximize(enemy, depth + 1, alpha, beta);
-                clearSnake(mySnake);
-                mySnake.undoMove();
-                updateBoard(mySnake);
+                //mySnake.undoMove();
+                //updateBoard(mySnake);
 
                 if ((bestMove == null) || (bestMove.returnValue < returnMove.returnValue)) {
                     bestMove = returnMove;
@@ -100,11 +98,9 @@ public class Minimax {
             List<Move> moves = getPossibleMoves(enemy.getHead(), true);
             if(moves.size() <= 0) System.out.println("Well fuck");
             for (Move currentMove : moves) {
-                clearSnake(enemy);
                 enemy.applyMove(currentMove);
                 updateBoard(enemy);
                 returnMove = maximize(mySnake, depth + 1, alpha, beta);
-                clearSnake(enemy);
                 enemy.undoMove();
                 updateBoard(enemy);
 
