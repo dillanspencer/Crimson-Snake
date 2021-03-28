092a07844c340e69350355e313395642985d2da6
Dillan Spencer <46987282+DillanSpencer@users.noreply.github.com>
2021-03-27 22:12:45 -0700

    Update Point.java

--- src/main/java/com/battlesnake/math/Point.java
+++ src/main/java/com/battlesnake/math/Point.java
@@ -2,7 +2,9 @@ package com.battlesnake.math;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class Point {
+import java.io.Serializable;
+
+public class Point implements Serializable {
 
     private int x;
     private int y;
