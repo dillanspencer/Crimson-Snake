22ce3e0d09c07c385fee54dce68f06e2affae65d
Dillan Spencer <46987282+DillanSpencer@users.noreply.github.com>
2021-03-27 18:35:16 -0700

    Update Minimax.java

--- src/main/java/com/battlesnake/minimax/Minimax.java
+++ src/main/java/com/battlesnake/minimax/Minimax.java
@@ -139,7 +139,7 @@ public class Minimax {
         }
         Point center = new Point(width/2, height/2);
         score -= (Math.abs(snake.getHead().getX() - center.getX()) + Math.abs(snake.getHead().getY()-center.getY())) * 2;
-        if(snake.getHead().getX() == 0 || snake.getHead().getY() == 0) score -= 1000;
+        if(head.getX() == 0 || head.getY() == 0 || head.getX() == width-1 || head.getY() == height-1) score -= 1000;
         //score += (Math.abs(snake.getHead().getX() - enemy.getHead().getX()) + Math.abs(snake.getHead().getY()-enemy.getHead().getY()));
         //if(Point.distance(snake.getHead(), center) < Point.distance(enemy.getHead(), center)) score += 100;
         for(Point f : food)
