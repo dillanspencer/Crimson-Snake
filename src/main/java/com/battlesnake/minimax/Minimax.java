7d3a333e54b4c72f1b9531db50fc508bcb505bad
Dillan Spencer <46987282+DillanSpencer@users.noreply.github.com>
2021-03-28 10:08:40 -0700

    Update Minimax.java

--- src/main/java/com/battlesnake/minimax/Minimax.java
+++ src/main/java/com/battlesnake/minimax/Minimax.java
@@ -142,6 +142,8 @@ public class Minimax {
         fillIn(board, regions, snake);
         Point head = snake.getHead();
 
+        System.out.println("Player Size: " + snake.length() + ", Enemy Size: " + enemy.length() + "Turn: " + turn);
+
         for (Map.Entry<Move, Point> move : Move.adjacent(head).entrySet()) {
             if (movable(board, move.getValue(), true)) {
                 playerReg += regions[move.getValue().getX()][move.getValue().getY()];
